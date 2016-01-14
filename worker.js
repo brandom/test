306c7d43c57a0465d14b1447e3ab066a5df5a651
Brandon Van Noy <bvannoy@me.com>
2016-01-14 12:41:25 -0600

    t

--- worker.js
+++ worker.js
@@ -1,19 +1,30 @@
 'use strict';
 var taglib = require('tagio');
 var a = require('async');
+var config = {
+  binaryDataDirectory: "/tmp",
+  binaryDataUrlPrefix: "/attachments",
+  binaryDataMethod: tagio.BinaryDataMethod.ABSOLUTE_URL,
+  apeSave: false,
+  id3v1Save: false,
+  id3v1Encoding: tagio.Encoding.UTF8,
+  id3v2Save: true,
+  id3v2Version: 4,
+  id3v2Encoding: tagio.Encoding.UTF8,
+  id3v2UseFrameEncoding: false
+};
 
 process.on('message', function(msg) {
   // console.log(msg);
   if (msg === 1) return process.exit();
   console.log('Received work from master');
   var q = a.queue(function (data, cb) {
-    console.log('New q with work', data.work.length);
     let tags = [];
     a.forEach(data.work, function(file, cb) {
       // console.log(file);
       try {
-        let info = taglib.open(file);
-        // tags.push(info.getAll());
+        let info = taglib.open(file, config);
+        tags.push(info.getAll());
 
       } catch(e) {
         cb(e);
