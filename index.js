e32fb679850011429950fe4db887d377424ab5cb
Brandon Van Noy <bvannoy@me.com>
2016-01-14 11:24:41 -0600

    more

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ for (var i = 0; i < workers; i++) {
   c[i] = 0;
   q[i] = [];
   w[i].on('message', function(msg) {
+    console.log('Done:', msg)
     processed = processed + msg;
     c[a] = c[a] - msg;
     if (c[a] === 0) w[a].send(1);
@@ -30,7 +31,7 @@ for (var i = 0; i < workers; i++) {
     //   w[a].send(work);
     // }
     let percent = Math.round(processed/total*100);
-    if (percent > lastp) console.log(percent,'%');
+    // if (percent > lastp) console.log(percent,'%');
     lastp = percent;
   })
 }
@@ -38,13 +39,15 @@ for (var i = 0; i < workers; i++) {
 walk(directory, /.mp3$/, function(err, results) {
   console.log('Chunking', results.length);
   total = results.length;
-  var i,j,p,chunk = 100;
+  var i,j,p,chunk = 1000;
   for (i=0, j=results.length, p=-1; i<j; i+=chunk) {
     p++;
     if (p >= w.length) p = 0;
     var work = results.slice(i,i+chunk);
     c[p] = c[p] + work.length;
     q[p].push(work)
+    w[p].send({concurrency: chunk});
+    console.log(chunk);
   }
 
   for (var i = 0; i < workers; i++) {
--- worker.js
+++ worker.js
@@ -2,26 +2,44 @@
 var taglib = require('tagio');
 var a = require('async');
 // var p = 0;
+var c = 0;
 
 process.on('message', function(msg) {
   // console.log(msg);
   if (msg === 1) return process.exit();
+  if (msg.concurrency) return c = msg.concurrency;
+
+  var q = a.queue(function (file, cb) {
+      let info = taglib.open(file);
+      cb(null, info);
+  }, c);
+
+  q.drain = function() {
+    console.log('all items have been processed');
+  }
   // console.log('Got', msg.length);
   msg.forEach(function(work) {
     let p = 0;
+    q.push(work, function(err) {
+      p++;
+      if (p === c) {
+        process.send(p);
+        p = 0;
+      }
+    });
     // let work = msg.pop();
     // console.log(work.length);
-    a.forEach(work, function(file, cb) {
-      var info = taglib.open(file);
-      // info.log();
-      // console.log(p);
-      cb();
-      p++;
-    }, function(err) {
-      if (err) console.log('Err', err);
-      process.send(p);
-      // p = 0;
-    });
+    // work.forEach(function(file, cb) {
+    //   var info = taglib.open(file);
+    //   // info.log();
+    //   // console.log(p);
+    //   cb(info.log());
+    //   p++;
+    // }, function(err, logs) {
+    //   if (err) console.log('Err', err);
+    //   process.send(p);
+    //   // p = 0;
+    // });
 
   });
 
